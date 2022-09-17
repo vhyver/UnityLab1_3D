0745040835ff7bf9aae62a8acfca8a3b031dd5bf
Vadym Verba <71729845+vhyver@users.noreply.github.com>
2022-09-17 20:53:34 +0300

    GameV1.0

--- Lab1_3D/Assets/Scripts/PlayerMovement.cs
+++ Lab1_3D/Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,42 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] float movementSpeed = 10f;
+    [NonSerialized] public float rotationSpeed = 90f;
+    private float gravity = -20f;
+    public float jumpHeight = 15f;
+
+    public CharacterController characterController;
+    Vector3 movementVector;
+    Vector3 rotationVector;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        var hInput = Input.GetAxis("Horizontal");
+        var vInput = Input.GetAxis("Vertical");
+
+        if (characterController.isGrounded)
+        {
+            movementVector = transform.forward * movementSpeed * vInput;
+            rotationVector = transform.up * rotationSpeed * hInput;
+            if (Input.GetButtonDown("Jump"))
+            {
+                movementVector.y = jumpHeight;
+            }
+        }
+
+        movementVector.y += gravity * Time.deltaTime;
+        characterController.Move(movementVector * Time.deltaTime);
+        transform.Rotate(rotationVector * Time.deltaTime);
     }
 }
